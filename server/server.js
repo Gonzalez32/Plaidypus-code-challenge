641543ff19ae4911e65da865d9f5d058207a14b6
Gonzalez32 <3232rose@gmail.com>
2021-10-14 20:40:40 -0500

    modified server - added more middleware-package

--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
-const express = require('express');
 require('dotenv').config();
-
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-// Route
-app.get("/getRestaurants", (req, res) => {
-    console.log("get all restaurants");
+app.use(morgan('dev'))
+
+
+
+// Get all Businesses
+app.get("/api/v1/getBusinesses", (req, res) => {
+    // console.log("get all businesses");
+    res.status(200).json({
+        status: "success",
+        data: {
+            businesses: ["handyman", "fixman"]
+        },
+    })
 });
+
+// Get a businesses
+app.get("/api/v1/business/:id", (req, res) => {
+    console.log(req.params);
+});
+
 
 const port = process.env.PORT || 3005;
 app.listen(3001, () => {
